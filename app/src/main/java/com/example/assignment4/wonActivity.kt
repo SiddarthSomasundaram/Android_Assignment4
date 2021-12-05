37201e345617951b4049309365767374aa677d92
unknown <Administrator@KANINI-LTP-480.gisbizdev.gisbiz.chennai.com>
2021-12-05 21:44:27 +0530

    first commit

---
+++ app/src/main/java/com/example/assignment4/wonActivity.kt
@@ -0,0 +1,14 @@
+package com.example.assignment4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class wonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_won)
+        val number = intent.getStringExtra("Number")
+        findViewById<TextView>(R.id.Number).text = "$number"
+    }
+}
